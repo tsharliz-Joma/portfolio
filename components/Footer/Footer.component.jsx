196f1eb45aa21be105ee945f4b67b926604f5a79
Charles Joma <almprincecharles@yahoo.com>
2024-03-22 13:16:48 +1100

    Additon

--- components/Footer/Footer.component.jsx
+++ components/Footer/Footer.component.jsx
@@ -1,24 +1,44 @@
-'use client';
-import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Grid from '@mui/material/Grid';
-import clsx from 'clsx';
-import style from './Footer.module.scss';
+"use client";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
+import clsx from "clsx";
+import style from "./Footer.module.scss";
+
+const GetInTouch = () => {
+  return (
+    <Box component="form" autoComplete="off">
+      <Box className={style.GetInTouch}>
+        <Box>
+          <Box>
+            <Typography variant="button">Say hey</Typography>
+          </Box>
+          <Box className={style.DisplayFlexCenterAround}>
+            <TextField className={style.Input} variant="outlined" label="email" size="small" />
+            <Button variant="contained">Send</Button>
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
 
 const Footer = () => {
   return (
     <Box className={style.Footer}>
       <Container className={style.Container}>
         <Grid container>
-          <Grid item xs={12}>
+          <Grid item xs={12} md={6}>
             <Grid container>
-              <Grid item xs={12}></Grid>
-              <Grid item sx={12}></Grid>
+              <Grid item xs={12} md={6}></Grid>
+              <Grid item sx={12} md={6}></Grid>
             </Grid>
           </Grid>
-          <Grid item>
-            <Box></Box>
+          <Grid item xs={12} md={6}>
+            <GetInTouch />
           </Grid>
         </Grid>
       </Container>
