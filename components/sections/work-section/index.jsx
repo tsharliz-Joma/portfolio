b7b61fdb0311e836e46bd794a19b94837e440959
Charles Joma <charles.joma@chepnetwork.com>
2024-07-22 17:07:21 +1000

    Vertical Timeline:: Added timeline components V1 + V2

--- components/sections/work-section/index.jsx
+++ components/sections/work-section/index.jsx
@@ -1,53 +1,16 @@
-"use client";
-import React, {forwardRef, useState} from "react";
+import React, { forwardRef } from "react";
 import content from "./work.json";
-import WorkExperienceCard from "@/components/ui/work-card";
-import {useGSAP} from "@gsap/react";
-import ScrollTrigger from "gsap/ScrollTrigger";
-import gsap from "gsap";
-import {generateRandomColors} from "@/lib/helper";
-import {generateColorsProp} from "@/lib/helper";
-gsap.registerPlugin(ScrollTrigger);
+import { VerticalTimelineV1 } from "@/components/ui/vertical-timeline";
 
-const ExperienceSection = forwardRef(({className, ...props}, ref) => {
+const ExperienceSection = forwardRef(({ className, ...props }, ref) => {
   const items = content.Work;
   const colors = content.colors;
-  const [bgColors, setBgColors] = useState([]);
-
-  // useGSAP(() => {
-  //   setBgColors(generateColorsProp(content, "Work", colors));
-  //   const tl = gsap.timeline({});
-
-  //   tl.from(".work-container", {
-  //     yPercent: -100,
-  //     opacity: 0,
-  //     ease: "power2.inOut",
-  //     scrollTrigger: {
-  //       trigger: ".work-container",
-  //       start: "top+=800 center",
-  //       scrub: true,
-  //     },
-  //   });
-  // }, []);
 
   return (
-    <section
-      id="Work Experience"
-      style={{
-        backgroundColor: `${
-          bgColors[Math.floor(Math.random() * colors.length)]
-        }`,
-      }}
-      className={`work-container z-[2] text-white relative py-20 text-center`}>
-      <div className="container">
-        <h1 className="text-4xl tracking-[2.5px]font-extrabold mb-8">
-          Work Experience
-        </h1>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 transition-opacity duration-700">
-          {items.map((item, index) => (
-            <WorkExperienceCard key={index} {...item} />
-          ))}
-        </div>
+    <section id="Work Experience" className={`z-[2] border-2 border-blue text-white relative py-20 text-center`}>
+      <div className="container border-2 border-red">
+        <h1 className="text-4xl tracking-[2.5px]font-extrabold mb-8">Work Experience</h1>
+        <VerticalTimelineV1 items={items} />
       </div>
     </section>
   );
---
+++ components/ui/vertical-timeline/index.tsx
@@ -0,0 +1,3 @@
+import { VerticalTimelineV1 } from "./VerticalTimelineV1";
+import { VerticalTimelineV2 } from "./VerticaltimelineV2";
+export { VerticalTimelineV1, VerticalTimelineV2 };
