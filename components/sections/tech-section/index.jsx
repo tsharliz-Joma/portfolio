2cc92c3991a2a5b17f25465d8d111a6783911fb7
Charles Joma <almprincecharles@yahoo.com>
2024-09-26 16:24:39 +1000

    Edit padding around tech stack section

--- components/sections/tech-section/index.jsx
+++ components/sections/tech-section/index.jsx
@@ -10,7 +10,7 @@ const TechStack = () => {
 
   return (
     <Section id="Tech Stack Section">
-      <div className="tech-container bg-transparent py-12 relative">
+      <div className="tech-container bg-transparent py-48 relative">
         <div className="container">
           <SectionHeader>Tech Stack</SectionHeader>
           <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6 justify-items-center">
