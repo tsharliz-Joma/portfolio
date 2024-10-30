7d45e12f6c8baed7d804258ff5b56b16eb70b62b
Charles Joma <almprincecharles@yahoo.com>
2024-10-30 17:11:05 +1100

    PROJECTS:: Change carousel to Portfolio display

--- components/sections/projects/Projects.jsx
+++ components/sections/projects/Projects.jsx
@@ -1,56 +1,48 @@
 "use client";
 import React from "react";
-import { motion } from "framer-motion";
+import {motion} from "framer-motion";
 import Image from "next/image";
-import { MoveUpRight } from "lucide-react";
-
-const projects = [
-  {
-    id: "01",
-    img: "https://images.unsplash.com/photo-1720048169707-a32d6dfca0b3?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    title: "Innovative Branding",
-    des: "Transforming your brand identity with creative solutions that resonate.",
-  },
-  {
-    id: "02",
-    img: "https://images.unsplash.com/photo-1709884735626-63e92727d8b6?q=80&w=1528&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    title: "Business Growth Strategies",
-    des: "Unlocking new avenues for business success through strategic development.",
-  },
-  {
-    id: "03",
-    img: "https://images.unsplash.com/photo-1572883454114-1cf0031ede2a?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxpaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    title: "Precision Engineering",
-    des: "Delivering high-quality solutions tailored to your specific needs.",
-  },
-  {
-    id: "04",
-    img: "https://images.unsplash.com/photo-1613919517761-0d9e719d3244?q=80&w=1469&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxpaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    title: "Digital Marketing Excellence",
-    des: "Crafting impactful marketing strategies that elevate your brand presence.",
-  },
-];
+import {MoveUpRight} from "lucide-react";
+import Data from "./Projects.content";
 
 export default function PortfolioGrid() {
+  const {Projects} = Data;
   return (
     <section>
       <div className="my-10">
-        <h1 className="text-2xl font-semibold text-center text-gray-800 capitalize lg:text-3xl dark:text-white">Our Blogs</h1>
+        <h1 className="text-2xl font-semibold text-center text-gray-800 capitalize lg:text-3xl dark:text-white">
+          My Projects
+        </h1>
 
-        <p className="mt-4 text-center text-gray-500 dark:text-gray-300">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum quam voluptatibus</p>
+        <p className="mt-4 text-center text-gray-500 dark:text-gray-300">
+          Projects that i am proud of and have built using full stack skills
+        </p>
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-8 px-5 pb-10 lg:px-16 lg:pb-16">
-        {projects.map((project) => (
-          <motion.article key={project.id} className="relative group shadow-lg overflow-hidden rounded-xl">
+        {Projects.map((project, index) => (
+          <motion.article
+            key={index}
+            className="relative group shadow-lg overflow-hidden rounded-xl">
             {/* Image with a slight hover scale effect */}
-            <motion.div whileHover={{ scale: 1.05 }} transition={{ type: "spring", stiffness: 300, damping: 20 }} className="h-72 w-full">
-              <Image src={project.img} alt={project.title} height={600} width={1200} className="h-full w-full object-cover transition-transform duration-500 ease-in-out rounded-xl" />
+            <motion.div
+              whileHover={{scale: 1.05}}
+              transition={{type: "spring", stiffness: 300, damping: 20}}
+              className="h-72 w-full">
+              <Image
+                src={`${project.img.src}`}
+                alt={project.title}
+                height={600}
+                width={1200}
+                className="h-full w-full object-cover transition-transform duration-500 ease-in-out rounded-xl"
+              />
             </motion.div>
 
             {/* Project Info Overlay */}
             <div className="absolute bottom-0 left-0 w-full bg-gradient-to-t from-black to-transparent p-6 opacity-0 group-hover:opacity-100 transition-opacity duration-500 ease-in-out">
               <h3 className="text-xl font-bold text-white">{project.title}</h3>
-              <p className="text-sm text-gray-300 mt-2">{project.des}</p>
+              <p className="text-sm text-gray-300 mt-2">
+                {project.description}
+              </p>
             </div>
 
             {/* Icon at the bottom right */}
--- components/sections/projects/index.jsx
+++ components/sections/projects/index.jsx
@@ -1,17 +1,14 @@
 import React from "react";
-import content from "./Projects.content.json";
-import { ProjectCarousel } from "@/components/ui/carousel/project-carousel";
 import Section from "@/components/ui/section";
 import SectionHeader from "@/components/ui/section-header";
+import PortfolioGrid from "./Projects";
 
 const Projects = () => {
-  const items = content.Projects;
-
   return (
     <Section id="Projects Section">
       <div className="container grid gap-14 py-20">
         <SectionHeader className="py-16">Projects</SectionHeader>
-        <ProjectCarousel items={items} className="" />
+        <PortfolioGrid />
       </div>
     </Section>
   );
